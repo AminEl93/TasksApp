311a65d45608c60527be091b922830323e033924
AminEl93 <amin_11_iverson@hotmail.com>
2024-01-30 00:54:15 +0100

    Editar y borrar una lista

--- src/app/components/lists/lists.component.ts
+++ src/app/components/lists/lists.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit, ViewChild } from '@angular/core';
+import { Router } from '@angular/router';
+import { AlertController, IonList } from '@ionic/angular';
+
+import { TasksService } from 'src/app/services/tasks.service';
+import { List } from 'src/app/models/list.model';
 
 @Component({
     selector: 'app-lists',
@@ -6,10 +11,61 @@ import { Component, OnInit } from '@angular/core';
 })
 
 export class ListsComponent  implements OnInit {
+    
+    @ViewChild(IonList) list!: IonList;
+    @Input() completedList = true;
+    
+    constructor(
+        public _tasksService: TasksService,
+        private _router: Router,
+        private _alertCtrl: AlertController
+    ) { }
 
-    constructor() { }
+    ngOnInit() { }
 
-    ngOnInit() {
+    // Lista seleccionada dependiendo de la pestaña en la que estés
+    selectedList(list: List) {
+        if (this.completedList) {
+            this._router.navigateByUrl(`/tabs/completed-tab/add-list/${list.id}`);
+        } else {
+            this._router.navigateByUrl(`/tabs/pending-tab/add-list/${list.id}`);
+        }    
+    }
 
+    // Editar una lista
+    async editList(list: List) {
+        const alert = await this._alertCtrl.create({
+            header: 'Editar lista',
+            inputs: [{
+                name: 'title',
+                type: 'text',
+                value: list.title,
+                placeholder: 'Nombre de la lista'
+            }],
+            buttons: [
+                {
+                    text: 'Cancelar',
+                    role: 'cancel',
+                    handler: () => {
+                        this.list.closeSlidingItems();
+                    }
+                },
+                {
+                    text: 'Actualizar',
+                    handler: (data) => {
+                        if (data.title.length === 0) return;            
+                        list.title = data.title;
+                        this._tasksService.saveStorage();
+                        this.list.closeSlidingItems();
+                    }
+                }
+            ]
+        });    
+        alert.present();    
+    }
+    
+    // Borrar una lista
+    deleteList(list: List) {    
+        this._tasksService.deleteList(list);    
     }
 }
