7348a0522ffb3e5b008c54924fec8ea64a3ba3b7
AminEl93 <amin_11_iverson@hotmail.com>
2024-01-30 00:50:27 +0100

    Servicio, pipe y estilos

--- src/app/services/tasks.service.ts
+++ src/app/services/tasks.service.ts
@@ -10,7 +10,41 @@ export class TasksService {
     public lists: List[] = [];
 
     constructor() {
-        console.log('Servicio de las tareas inicializado!')
+        this.loadStorage();
+    }
+
+    // Crear una lista
+    createList(title: string){
+        const newList = new List(title);
+        this.lists.push(newList);
+        this.saveStorage();
+        return newList.id;
+    }    
+    
+    // Obtener una lista 
+    getList(id: string | number) {
+        id = Number(id);
+        return this.lists.find(listData => listData.id === id);
+    }
+
+    // Borrar una lista
+    deleteList(list: List) {
+        this.lists = this.lists.filter(listData => listData.id !== list.id);    
+        this.saveStorage();    
+    }
+
+    // Persistencia local: Guardar el storage
+    saveStorage() {
+        localStorage.setItem('data', JSON.stringify(this.lists));    
     }
     
+    // Persistencia local: Cargar el storage
+    loadStorage() {
+        const lists = localStorage.getItem('data');
+        if (lists) {
+            this.lists = JSON.parse(lists);
+        } else {
+            this.lists = [];
+        }
+    }
 }
